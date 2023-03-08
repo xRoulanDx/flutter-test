4474d9dd91f9ac37903de5e976a2ecfad2040de3
Коноплев Владимир Андреевич <v.konoplev@tinkoff.ru>
2023-03-08 22:58:32 +0300

    Finisg guide

--- lib/main.dart
+++ lib/main.dart
@@ -28,33 +28,129 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
+  var favorites = <WordPair>[];
+
   void getNext() {
     current = WordPair.random();
+
+    notifyListeners();
+  }
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+
+    notifyListeners();
+  }
+
+  void removeFav(WordPair fav) {
+    favorites.remove(fav);
+
     notifyListeners();
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritePage();
+        break;
+      default:
+        throw UnimplementedError('no page for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, container) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: container.maxWidth > 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BigCard(pair: pair),
+        SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
+            ElevatedButton.icon(
+              onPressed: () {
+                appState.toggleFavorite();
+              },
+              icon: Icon(icon),
+              label: Text('Like'),
+            ),
+            SizedBox(width: 10),
             ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'))
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
           ],
         ),
-      ),
+      ],
     );
   }
 }
@@ -79,6 +175,38 @@ class BigCard extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         child: Text("${pair.first} ${pair.second}", style: style),
       ),
+    );
+  }
+}
+
+class FavoritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var favs = appState.favorites;
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('There is not favorite words yet'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have ${favs.length} favorite words'),
+        ),
+        for (var fav in favs)
+          ListTile(
+            title: Text(fav.asCamelCase),
+            trailing: ElevatedButton(
+                onPressed: () {
+                  appState.removeFav(fav);
+                },
+                child: Text('Remove')),
+          )
+      ],
     );
   }
 }
